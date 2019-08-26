daf711630d28ff0ac76c3b7d97e9036497d3a3e7
cocodemus <54139095+cocodemus@users.noreply.github.com>
2019-08-26 17:54:24 +0700

    Update liveCode1.js

--- liveCode1.js
+++ liveCode1.js
@@ -5,5 +5,5 @@ else if (nilai >=65 && nilai <=79 ) 'grade = B';
 else if (nilai >=50 && nilai <= 64 ) 'grade = C';
 else if (nilai >=35 && nilai <= 49 ) 'grade = D';
 else if (nilai >=0 && nilai <= 34 )  'grade = 'E';
-else (nilai <0 && nilai >100) 'Nilai Invalid';
+else if (nilai <0 && nilai >100) 'Nilai Invalid';
 print nama, nilai;
